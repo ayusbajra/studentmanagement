e63fabfbb13f4ecc127ed12199180ce100b7fb62
Ayush Bajracharya <ayus.bajra@gmail.com>
2024-06-19 18:12:09 +0545

    - register 'students' app in settings.py - add model classes(Student, Course, Instructor, Enrollment) in models.py

--- students/models.py
+++ students/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_code = models.CharField(max_length=25, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.course_name
+
+
+class Instructor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    courses = models.ManyToManyField(Course, related_name='instructors')
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=2)
+    exam_score = models.FloatField()
+
+    def __str__(self):
+        return '{} - {}'.format(self.student, self.course)
