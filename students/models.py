fa4478fe24992550e37d8ad26d2f4c0d0bb2c477
Ayush Bajracharya <ayus.bajra@gmail.com>
2024-06-20 13:34:48 +0545

    - use custom db table names for model

--- students/models.py
+++ students/models.py
@@ -7,6 +7,9 @@ class Student(models.Model):
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField()
 
+    class Meta:
+        db_table = 'student'
+
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
@@ -15,6 +18,9 @@ class Course(models.Model):
     course_name = models.CharField(max_length=100)
     course_code = models.CharField(max_length=25, unique=True)
     description = models.TextField()
+
+    class Meta:
+        db_table = 'course'
 
     def __str__(self):
         return self.course_name
@@ -26,6 +32,9 @@ class Instructor(models.Model):
     email = models.EmailField(unique=True)
     courses = models.ManyToManyField(Course, related_name='instructors')
 
+    class Meta:
+        db_table = 'instructor'
+
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
@@ -35,6 +44,9 @@ class Enrollment(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     grade = models.CharField(max_length=2)
     exam_score = models.FloatField()
+
+    class Meta:
+        db_table = 'enrollment'
 
     def __str__(self):
         return '{} - {}'.format(self.student, self.course)
