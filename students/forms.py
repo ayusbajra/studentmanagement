90a4c8968872c749dc2d11d59888993e1d32d887
Ayush Bajracharya <ayus.bajra@gmail.com>
2024-06-22 18:09:46 +0545

    - add template, for course, instructor, enrollment models - show courses in student list

--- students/forms.py
+++ students/forms.py
@@ -12,15 +12,25 @@ class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ['course_name', 'course_code', 'description']
+        widgets = {
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+        }
 
 
 class EnrollmentForm(forms.ModelForm):
     class Meta:
         model = Enrollment
         fields = ['student', 'course', 'grade', 'exam_score']
+        widgets = {
+            'student': forms.Select(attrs={'class': 'form-control'}),
+            'course': forms.Select(attrs={'class': 'form-control'}),
+        }
 
 
 class InstructorForm(forms.ModelForm):
     class Meta:
         model = Instructor
         fields = ['first_name', 'last_name', 'email', 'courses']
+        widgets = {
+            'courses': forms.CheckboxSelectMultiple,
+        }
